858e8641c00b1e2bd6e43b4d6ba992f1e800a7bf
thepatrick00 <thepatrick17@gmail.com>
2023-03-23 15:23:19 -0500

    lifted count state up from each FancyLink and create Counters state instead because I needed the ability to reset all the counters at once

--- src/components/FancyLink.jsx
+++ src/components/FancyLink.jsx
@@ -1,43 +1,28 @@
 import React from 'react'
 import styles from './FancyLink.module.css'
 
-function FancyLink({ url, setTotal, index }) {
-  const savedCounter = JSON.parse(
-    localStorage.getItem(`counter ${index}`)
-  )
-
-  const [counter, setCounter] = React.useState(savedCounter || 0)
-
-  const handleClick = () => {
-    setCounter(counter + 1)
-    setTotal(curr => curr + 1)
-  }
-
+function FancyLink({ link, updateCounter, count }) {
+  // const savedCounter = JSON.parse(
+  //   localStorage.getItem(`counter ${index}`)
+  // )
   
-  React.useEffect(() => {
-    localStorage.setItem(
-      `counter ${index}`,
-      JSON.stringify(counter)
-    )
-
-    // This cleanup function is used for debugging purpose only
-    // return () => {
-    //   localStorage.removeItem(`counter ${index}`)
-    // }
-  }, [counter])
-
-  // also open links in new tab by default
+  // React.useEffect(() => {
+  //   localStorage.setItem(
+  //     `counter ${index}`,
+  //     JSON.stringify(counter)
+  //   )
+  // }, [counter])
 
   return (
     <a
       className={styles.wrapper}
-      onClick={handleClick}
-      href={url}
+      onClick={updateCounter}
+      href={link}
       target="_blank"
       rel="noreferrer"
     >
-      <div className={styles.counter}>{counter}</div>
-      <p className={styles.url}>{url}</p>
+      <div className={styles.counter}>{count}</div>
+      <p className={styles.url}>{link}</p>
     </a>
   )
 }
--- src/components/LinkBox.jsx
+++ src/components/LinkBox.jsx
@@ -4,36 +4,70 @@ import styles from './LinkBox.module.css'
 import FancyLink from './FancyLink'
 import Tooltip from './Tooltip'
 import ProgressBar from './ProgressBar'
+// import { refresh-cw } from 'react-feather'
 
-const LINKS = [
+let LINKS = [
   'https://www.swagbucks.com/g/l/p3btd7',
   'https://www.swagbucks.com/g/l/6vyye1',
   'https://www.swagbucks.com/g/l/xcq6yq',
   'https://www.swagbucks.com/g/l/1j26i4',
 ]
 
-// const LINKS = [
-//   'https://www.example.com/',
-//   'https://www.example.com/',
-//   'https://www.example.com/',
-//   'https://www.example.com/',
-// ]
+LINKS = [
+  'https://www.example.com/',
+  'https://www.example.com/',
+  'https://www.example.com/',
+  'https://www.example.com/',
+]
+
+const initialCounters = LINKS.map((link) => {
+  return {
+    link: link,
+    count: 0,
+    uniqueId: Math.random(),
+  }
+})
 
 const savedTotal = JSON.parse(localStorage.getItem('total'))
+const savedCounters = JSON.parse(localStorage.getItem('counters'))
 
-function LinkBox({className}) {
+function LinkBox({ className }) {
   const [total, setTotal] = React.useState(savedTotal || 0)
+  const [counters, setCounters] = React.useState(
+    savedCounters || initialCounters
+  )
+
+  const updateCounter = (uniqueId) => {
+    const nextCounters = counters.map((link) => {
+      // if the uniqueId that is passed in
+      // matches the uniqueId we currently are looking for
+      // in this .map(), then update this object.
+      if (link.uniqueId === uniqueId) {
+        return { ...link, count: link.count + 1 }
+      } else {
+        return link
+      }
+    })
+    setCounters(nextCounters)
+    setTotal(total + 1)
+  }
+
+  const handleReset = () => {
+    setTotal(0)
+    setCounters(initialCounters)
+  }
 
   React.useEffect(() => {
-    // When total changes, put it in localStorage
     localStorage.setItem('total', JSON.stringify(total))
-  }, [total])
+    localStorage.setItem('counters', JSON.stringify(counters))
+  }, [total, counters])
 
   let info =
     total < 4 ? (
       <>
         <p className={styles.info}>
-          <b>1st search win</b> Search each link once to get a search win
+          <b>1st search win</b> Search each link once to get a search
+          win
         </p>
         <Tooltip>
           It must be your first daily search since midnight
@@ -42,8 +76,8 @@ function LinkBox({className}) {
     ) : (
       <>
         <p className={styles.info}>
-          <b>2nd Search Win:</b> Search 4 links 8x + 1 
-          to get your second search win. This is a total of 33 searches.
+          <b>2nd Search Win:</b> Search 4 links 8x + 1 to get your
+          second search win. This is a total of 33 searches.
         </p>
         <Tooltip>
           Sometime it may take 40 search trys to get your second
@@ -52,7 +86,7 @@ function LinkBox({className}) {
       </>
     )
 
-      const nextClass = `${styles.wrapper} ${className}`
+  const nextClass = `${styles.wrapper} ${className}`
 
   return (
     <main className={nextClass}>
@@ -61,20 +95,20 @@ function LinkBox({className}) {
 
       <div className={styles.linksWrapper}>
         <div className={styles.clicks}>Clicks</div>
-        {LINKS.map((link, i) => {
+        {counters.map(({ link, count, uniqueId }) => {
           return (
             <FancyLink
-              key={i}
-              index={i}
-              url={link}
-              setTotal={setTotal}
+              key={uniqueId}
+              link={link}
+              updateCounter={() => updateCounter(uniqueId)}
+              count={count}
             />
           )
         })}
       </div>
 
       <div className={styles.totalWrapper}>
-        <div>
+        <div onClick={handleReset}>
           Total
           <div className={styles.totalBox}>{total}</div>
         </div>
